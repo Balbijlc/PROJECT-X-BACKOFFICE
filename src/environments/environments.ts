c7feae1eedc6afdcb4d74e6f43875aaf4ef83faf
root <root@BalbiJLC>
2023-12-09 01:25:03 -0300

    commit de guardar

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,13 +1,13 @@
 export const environments = {
     production: false,
-    apiUrl: 'http://localhost:4000/api/cars', 
+    // apiUrl: 'http://localhost:4000/api/cars', 
 
 
-    // apiUrl: 'https://projectx.sicrux.io/api/cars',
+    apiUrl: 'https://projectx.sicrux.io/api/cars',
 
 
-    baseUrl: 'http://localhost:4000',  
-    // baseUrl: 'https://projectx.sicrux.io',
+    // baseUrl: 'http://localhost:4000',  
+    baseUrl: 'https://projectx.sicrux.io',
 
 
 }
