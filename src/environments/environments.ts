055da5948a8ad70e113b8dd7562bf26fd0f328bb
root <root@BalbiJLC>
2023-12-09 08:15:15 -0300

    commit

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,15 +1,15 @@
 export const environments = {
-    production: false,
-    apiUrl: 'http://localhost:4000', 
+    production: true,
+    // apiUrl: 'http://localhost:4000', 
 
 
     // apiUrl: 'https://projectx.sicrux.io/api/cars',
-    // apiUrl: 'https://projectx.sicrux.io',
+    apiUrl: 'https://projectx.sicrux.io',
 
 
 
-    baseUrl: 'http://localhost:4000',  
-    // baseUrl: 'https://projectx.sicrux.io',
+    // baseUrl: 'http://localhost:4000',  
+    baseUrl: 'https://projectx.sicrux.io',
 
 
 
